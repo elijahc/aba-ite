d63ea80e749ee09105316330b2158c62320bf61f
Elijah <ejd.christensen@gmail.com>
2024-03-15 06:00:02 +0000

    Changed csv headers and plot format

--- Hello.py
+++ Hello.py
@@ -14,7 +14,7 @@
 
 import streamlit as st
 import pandas as pd
-import seaborn as sns
+import altair as alt
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
@@ -30,11 +30,25 @@ def run():
 
     st.sidebar.success("Select a demo above.")
 
-    df = pd.read_csv('./2024.ITE.scaled.csv')
-    
-    
+    df = pd.read_csv('./2024.ITE.scaled.csv').sort_values(by='Score').reset_index(drop=True)
+    ite_cdf = df.set_index('Score')
+    # st.table(ite_cdf.reset_index())
+    data = pd.melt(ite_cdf.reset_index(), id_vars=['Score']).rename(columns={'value':'Percentile','variable':'year'})
+    ite_pdf = ite_cdf.diff()
+    pdf_data = pd.melt(ite_pdf.reset_index(), id_vars=['Score']).rename(columns={'value':'Percentile','variable':'year'})
 
-    st.table(df)
+    # data = data.groupby('year').transform(lambda df: df.sort_values(by=['Percentile','Score']).diff())
+
+    chart = alt.Chart(data.query("Score > 10")).mark_line().encode(
+        x='Score:Q',
+        y='Percentile:Q',
+        color='year'
+    )    
+
+    st.altair_chart(chart,use_container_width=True)
+    # st.table(data.Percentile.diff())
+
+
 
 if __name__ == "__main__":
     run()
